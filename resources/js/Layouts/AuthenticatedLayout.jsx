c4837b17272e72fa3070d566fd76ed5750fe4799
welldanyogia <welldanyogia01@gmail.com>
2025-04-06 15:07:42 +0700

    update

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -5,9 +5,9 @@ import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import {Link, usePage} from '@inertiajs/react';
 import {useState} from 'react';
 
-export default function AuthenticatedLayout({header, children}) {
-    const user = usePage().props.auth.user;
-    console.log(user)
+export default function AuthenticatedLayout({header, children,auth}) {
+    const user = auth.user;
+    console.log(auth)
     const role = user.roles[0].name;
 
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
